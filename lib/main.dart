884628008855e28deba266c6cda803a1d9387d84
Yoshiaki Ishikawa / 石川　善章 <yoshiaki.ishikawa.j6w@densotechno.com>
2020-02-07 17:38:09 +0900

    display leaflet map

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
@@ -39,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _load() async {
     final res = await http.post('https://tokogeko.net/api/login',
-        body: {'userid': 'test', 'password': 'test123'});
+        body: {'userid': 'xxx', 'password': 'xxx'});
     setState(() {
       _data = res.headers['authorization'];
     });
@@ -47,11 +49,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new LatLng(35.000081, 137.004055),
+        zoom: 16.0,
       ),
-      body: Text(_data),
+      layers: [
+        new TileLayerOptions(
+          urlTemplate: "https://api.tiles.mapbox.com/v4/"
+              "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+          additionalOptions: {
+            'accessToken':
+                'pk.eyJ1IjoieWlzaWthd2EiLCJhIjoiY2s2YndmdXFuMGZ1bDNsb3ZnMXBsbnI3eSJ9.gftC8NPsB9xNWIVEdWnTvw',
+            'id': 'mapbox.streets',
+          },
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 20.0,
+              height: 20.0,
+              point: new LatLng(35.000081, 137.004055),
+              builder: (ctx) => new Container(
+                child: new FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
